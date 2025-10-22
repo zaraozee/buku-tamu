5ff65d2d734e6739a9dc458824fe086e35d639d0
= <=>
2025-10-22 21:02:43 +0700

    menambahkan halaman hapus tamu dan fitur hapus data tamu

---
+++ hapus-tamu.php
@@ -0,0 +1,17 @@
+<?php
+// Panggil file function.php
+require_once 'function.php';
+
+// Jika ada id yang dikirim lewat URL
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    if (hapus_tamu($id)) {
+        // Jika data berhasil dihapus
+        echo "<script>alert('Data Berhasil dihapus!');</script>";
+        echo "<script>window.location.href='buku-tamu.php';</script>";
+    } else {
+        // Jika gagal dihapus
+        echo "<script>alert('Data Gagal dihapus!');</script>";
+    }
+}
+?>
