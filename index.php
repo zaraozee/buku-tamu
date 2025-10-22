1845c01be513a5a243986afde462614837e94cd9
= <=>
2025-10-22 20:50:08 +0700

    templating app buku tamu dengan sb admin 2

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include_once('templates/header.php');
+?>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Dashboard Admin</h1>
+
+</div>
+<!-- /.container-fluid -->
+
+
+<?php
+include_once('templates/footer.php');
+?>
